31d288fc5a10d86a61a0af0f64d57fd2e5683608
labocho <labocho@penguinlab.jp>
2026-05-21 11:34:00 +0900

    add rubocop-rake, rubocop-rspec

--- spec/polyssh_spec.rb
+++ spec/polyssh_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe Polyssh do
   it "has a version number" do
-    expect(Polyssh::VERSION).not_to be nil
+    expect(Polyssh::VERSION).not_to be_nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    pending "unimplemented"
   end
 end
